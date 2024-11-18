a718c97e8bf235e4e78df6e0f878364008c642b9
SpeakBoy <nevinspeakman@outlook.com>
2024-11-18 16:52:13 -0500

    Remove offscreen bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,7 +28,13 @@ class AlienInvasion:
         while True:
             self._check_events() 
             self.ship.update()    
-            self.bullets.update()       
+            self.bullets.update() 
+            
+            # Get rid of bullets that have disappeared.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet) 
+
             self._update_screen()
             self.clock.tick(60)
     
