13f745c6fad2f51abee42b81819857d0ca9ec431
SpeakBoy <nevinspeakman@outlook.com>
2024-11-25 05:40:08 -0500

    Add level display

--- alien_invasion.py
+++ alien_invasion.py
@@ -78,6 +78,7 @@ class AlienInvasion:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.sb.prep_score()
+            self.sb.prep_level()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens.
@@ -144,6 +145,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
     
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
